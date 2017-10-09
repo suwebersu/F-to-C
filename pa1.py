41494beb9f2df5848fe371fae1d93525d441f9f8
suwebersu <32618873+suwebersu@users.noreply.github.com>
2017-10-08 17:20:38 -0700

    Create pa1.py

---
+++ pa1.py
@@ -0,0 +1,5 @@
+#Weber Su
+print("What is the temperature in Fahrenheit?")
+f = int(input())
+c = (((f-32)*5)/9)
+print("The temperature ", f, "degrees Fahrenheit(F) is equivalent to ", c, " degree in Celcius(C)")
